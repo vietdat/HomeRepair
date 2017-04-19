01dac2bf076f1c36f8e558e2e76003849866fe28
Ubuntu <ubuntu@ip-172-31-10-15.us-west-2.compute.internal>
2017-04-19 23:22:05 +0000

    upload image success

--- routes/Admin/gioithieu.js
+++ routes/Admin/gioithieu.js
@@ -1,84 +1,96 @@
 var express = require('express');
 var router = express.Router();
-var Introduction=require('../../model/gioithieu.model');
+var Introduction = require('../../model/gioithieu.model');
 var decode = require('decode-html');
-
+var Busboy = require('busboy')
+var fs = require('fs')
+var path = require('path')
 /* GET home page. */
 router.get('/', function(req, res, next) {
     //get category list
-    Introduction.find(function(err,IntroductionList){
-        if (err) throw err;
-        else{
-            IntroductionList.forEach(function(element) {
-                console.log(decode(element.content));
-                element.content=decode(element.content);
-            }, this);
-            res.locals.IntroductionList=IntroductionList;
-            res.render('Admin/gioithieu',{layout:'Admin/layout'});
-        }
+    Introduction.find(function(err, IntroductionList) {
+	if (err) throw err;
+	else {
+	    IntroductionList.forEach(function(element) {
+		element.content = decode(element.content);
+	    }, this);
+	    res.locals.IntroductionList = IntroductionList;
+	    res.render('Admin/gioithieu', {
+		layout: 'Admin/layout'
+	    });
+	}
     })
- // res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
 });
-router.post('/add',function(req,res,next){
-    console.log(req.body);
+router.post('/add', function(req, res, next) {
+    var title = "";
+    var url = "";
+    var altImage = "";
+    var urlImage = "";
+    var description = "";
+    var content = "";
+    var busboy = new Busboy({
+	headers: req.headers
+    });
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+	urlImage = filename;
+	file.pipe(fs.createWriteStream(path.join(process.env.PWD, 'public/images', filename)));
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+	console.log('Field [' + fieldname + ']: value: ');
+    });
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+	switch (fieldname) {
+	case 'title':
+	    title = val;
+	    break;
+	case 'url':
+	    url = val;
+	    break;
+	case 'altImage':
+	    altImage = val;
+	    break;
+	case 'description':
+	    description = val;
+	    break;
+	case 'content':
+	    content = val;
+	    break;
+	default:
+	    break;
+	}
+    });
+    busboy.on('finish', function() {
+	var item = new Introduction({
+	    url: url,
+	    title: title,
+	    content: content,
+	    image: {
+		'alt': altImage,
+		'src': '/images/' + urlImage
+	    },
+	    description: description
+	});
 
-    if(req.body.url && req.body.url==="") {
-        console.log("Missing url");
-        return;
-    }
-
-    if(req.body.title && req.body.title === "") {
-        console.log("Missing title");
-        return;
-    }
-
-    if(req.body.content && req.body.content === "") {
-        console.log("Missing content");
-        return;
-    }
-
-    if(req.body.alt && req.body.altImage === "") {
-        console.log("Missing altImage");
-        return;
-    }
-
-    if(req.body.urlImage && req.body.urlImage === "") {
-        console.log("Missing urlImage");
-        return;
-    }
-
-    if(req.body.description && req.body.description === "") {
-        console.log("Missing description");
-        return;
-    }
-
-    var item=new Introduction({
-        url:req.body.url,
-        title:req.body.title,
-        content:req.body.content,
-        image:{
-            'alt':req.body.altImage,
-            'src':req.body.urlImage
-        },
-        description:req.body.description
+	item.save(function(err) {
+	    if (err) throw err;
+	    else res.redirect('/hungthinh-admin/gioi-thieu');
+	})
+    });
+    req.pipe(busboy);
+})
+router.post('/delete', function(req, res, next) {
+    var id = req.body.id;
+    Introduction.remove({
+	_id: id
+    }, function(err) {
+	console.log(err);
+	if (err) {
+	    res.send(err);
+	} else {
+	    res.send('success');
+	}
     });
 
-    item.save(function(err){
-  if (err) throw err;
-  else res.redirect('/hungthinh-admin/gioi-thieu');
-    })
-})
-router.post('/delete',function(req,res,next){
-    var id=req.body.id;
-    Introduction.remove({_id:id},function(err){
-        console.log(err);
-        if (err){
-            res.send(err);
-        }
-        else{
-            res.send('success');
-        }
-    });    
-    
 })
 module.exports = router;
