3c213e6e69685d7743b8c9b334b6a5688771dbdf
Đạt Lê <datle@goappable.com>
2017-04-25 23:07:46 +0700

    edit title

--- routes/mangluoi.route.js
+++ routes/mangluoi.route.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
         console.log("Config ", config );
         linkbar[0] = config.domain;
         linkbar[1] = linkbar[0] + "/mang-luoi";
-        
+
         var html = "<div>"
                     + '<a href=' + linkbar[0] + '>Trang chủ</a>' + "/"
                     + '<a href=' + linkbar[1] + '>Mạng lưới</a>'
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
         done(null, html);
     },
     getData: function(done) {
-      MangLuoiModel.aggregate([ { $project : { _id: 0, "url":1, "image":1, "title":1, "description": 1} } 
+      MangLuoiModel.aggregate([ { $project : { _id: 0, "url":1, "image":1, "title":1, "description": 1} }
               ]).exec(done);
     },
     content: ['getData', function(data, done) {
@@ -42,13 +42,13 @@ router.get('/', function(req, res, next) {
 
             for(var i = 0; i < length; i++) {
               if(i === 0) {
-                html_head = html_head 
+                html_head = html_head
                         + '<div class="col-md-3">'
                         +    '<a href="'+ config.domain + "/mang-luoi/" + data.getData[i].url +'">'
                         +         '<img width="150" height="150" src="'+ data.getData[i].image.src +'" class="aligncenter wp-post-image" alt="'+data.getData[i].image.alt+'">'
                         +     '</a>'
                         + '</div>'
-                        + '<div class="col-md-9 no-padding-left">'          
+                        + '<div class="col-md-9 no-padding-left">'
                         +     '<header>'
                         +        '<h3 class="no-margin-top">'
                         +             '<a href="'+ config.domain + "/mang-luoi/" + data.getData[i].url +'"><h3>'+data.getData[i].title+'</h3></a>'
@@ -62,7 +62,7 @@ router.get('/', function(req, res, next) {
                         +  '</div>'
               }
               else {
-                html = html 
+                html = html
                 + '<div class="col-sm-12 margin-buttom-10 no-padding-left">'
                 +    '<div class="row">'
                 +     '<div class="col-sm-2">'
@@ -129,11 +129,16 @@ router.get('/:url', function(req, res, next) {
         linkbar[0] = config.domain;
         linkbar[1] = linkbar[0] + "/mang-luoi";
         linkbar[2] = linkbar[1] + "/" + req.params.url;
-        
+
+        var first = data.data.title.substring(0,1);
+        var last = data.data.title.substring(1);
+        var title = first.toUpperCase() + last.toLowerCase();
+
+
         var html = "<div>"
                     + '<a href=' + linkbar[0] + '>Trang chủ</a>' + "/"
                     + '<a href=' + linkbar[1] + '>Mạng lưới</a>' + "/"
-                    + '<a href=' + linkbar[2] + '>' + data.data.title +'</a>'
+                    + '<a href=' + linkbar[2] + '>' + title +'</a>'
                     + '</div>';
         done(null, html);
       } else {
@@ -141,7 +146,7 @@ router.get('/:url', function(req, res, next) {
       }
     }],
     getDataFooter: function(done) {
-      MangLuoiModel.aggregate([ { $project : { _id: 0, "url":1, "image":1, "title":1, "description": 1} } 
+      MangLuoiModel.aggregate([ { $project : { _id: 0, "url":1, "image":1, "title":1, "description": 1} }
               ]).exec(done);
     },
     footer: ['getDataFooter', function(data, done) {
@@ -157,7 +162,7 @@ router.get('/:url', function(req, res, next) {
             }
 
             for(var i = 0; i < length; i++) {
-              html = html 
+              html = html
                 + '<div class="col-sm-12 margin-buttom-10 no-padding-left">'
                 +    '<div class="row">'
                 +     '<div class="col-sm-2">'
@@ -205,7 +210,7 @@ router.get('/:url', function(req, res, next) {
             for(var i = 0; i < length; i++) {
               html = html
                       + '<li><a href="#">'+ data.getDataFooter[i].title +'</a></li>'
-            }     
+            }
           }
 
           html = html + '</ul>';
@@ -230,7 +235,7 @@ router.get('/:url', function(req, res, next) {
     } else {
       return;
     }
-    
+
   })
 });
 
