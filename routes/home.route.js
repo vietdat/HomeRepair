8876497e6897b8be5e885c8fb09cf5e67f093251
Đạt Lê <datle@goappable.com>
2017-04-27 22:05:55 +0700

    fix update don gia

--- routes/home.route.js
+++ routes/home.route.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var async = require('async');
+var DonGiaModel = require('../model/dongia.model');
+
 
 router.get('/', function(req, res, next) {
   res.render('Home/homepage', {
@@ -10,11 +12,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/home', function(req, res, next) {
- 	res.render('Home/homepage', {
-  	title: "Sửa chữa cải tạo Hưng Thịnh",
-  	body: "body"
-  });
-});
+// router.get('/home', function(req, res, next) {
+//   async.auto({
+//     // dongia:function(done) {
+//     //   DonGiaModel.find().lean().exec(done);
+//     // },
+//     // dongiasuachuacaitao: ['dongia',function(data, done) {
+//     //   forEach
+//     // }],
+//     // dongiaxaydungmoi:['dongia',function(data, done) {
+//     //
+//     // }],
+//
+//
+//   });
+//  	res.render('Home/homepage', {
+//   	title: "Sửa chữa cải tạo Hưng Thịnh",
+//   	body: "body"
+//   });
+// });
 
 module.exports = router;
