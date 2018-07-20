404fbc279a9fd1608199d8d09139dfb44c9f9218
NhaTD <nhatran406@gmail.com>
2018-07-20 16:01:53 +0700

    ds

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+ // 	"domain" : "http://localhost:3000"
+ 	// "domain" : "http://houserepair.herokuapp.com"
+ 	"domain" : "http://xaydungcaitao.com"
+
+};
+
+module.exports = config;
